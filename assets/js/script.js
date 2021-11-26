b0a27885cbc14be428ae07b84ced5567a26e3587
PaulaCM <pcmarginean@yahoo.com>
2021-11-26 09:20:41 +0000

    Buttons

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+
+
+let rockBtn = document.getElementsByClassName("rock-btn");
+let scissorsBtn = document.getElementsByClassName("scissors-btn");
+let paperBtn = document.getElementsByClassName("paper-btn");
+let possibleChoice = document.getElementsByTagName("button");
+
+rockBtn.addEventListener("click", function () {
+
+}
+
+
+
+
+
